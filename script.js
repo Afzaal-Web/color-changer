b8a66501bb090bf1063850c1d6ab2c99b2c7dab5
Afzaal-Web <afzaalwebsolutions@gmail.com>
2025-05-29 19:44:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -61,7 +61,11 @@ bodyColor.innerText = colors[reverseColor].name;;
     }
   }
   );
-
+ document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.key === 'Enter') {
+      btnColorChanger.click(); // Triggers the same color change logic
+    }
+  });
 
 //   Random color Generator 
 
